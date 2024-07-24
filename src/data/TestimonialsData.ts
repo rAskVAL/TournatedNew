a164b1c3bd64f2fc18ad2bf9710833876a845717
VertexoSports <nick@vertexo.net>
2024-07-24 18:11:42 +0300

    Update Testimonials data (updated reviews)

--- src/data/TestimonialsData.ts
+++ src/data/TestimonialsData.ts
@@ -1,13 +1,13 @@
 export const testimonialsData = [
   {
     en: {
-      text: "Tournated.com is a game-changer for tennis tournaments. Its intuitive interface, seamless registration, and real-time analytics make it a must-have tool for tennis enthusiasts. Highly recommended!",
+      text: "Working with the Tournated team has been a game-changer for us as we digitalize and modernize all processes and activities of the Latvian Tennis Federation. Their expertise and support have exceeded our expectations, and we are highly satisfied with the progress and developments thus far.",
       name: "Edgars Rauza",
       role: "CFO of LTS",
       avatar: "https://i.imgur.com/G2gnTcN.png",
     },
     lv: {
-      text: "Tournated.com ir spēļu mainītājs tenisa turnīriem. Tā intuitīvais interfeiss, nevainojama reģistrācija un reāllaika analītika padara to par obligātu rīku tenisa entuziastiem. Ļoti ieteicams!",
+      text: "Darbs ar Tournated komandu ir ievērojami uzlabojis Latvijas Tenisa federācijas darbību, jo viņu sniegtais risinājums ir digitalizējis un modernizējis visus mūsu procesus un aktivitātes. Komandas plašās zināšanas un nepārtrauktā atbalsta kvalitāte ir pārspējušas mūsu cerības. Mēs esam ārkārtīgi apmierināti ar līdzšinējo progresu un attīstību, un mēs ar prieku gaidām turpmāku sadarbību, kas noteikti veicinās mūsu federācijas izaugsmi.",
       name: "Edgars Rauza",
       role: "LTS CFO",
       avatar: "https://i.imgur.com/G2gnTcN.png",
@@ -15,13 +15,13 @@ export const testimonialsData = [
   },
   {
     en: {
-      text: "Tournated.com revolutionized our tournament management, streamlining processes. Highly recommended for seamless events.",
+      text: "Thanks to Tournated's solution, we were finally able to launch our own sports management platform and national tour, significantly accelerating the growth of tennis in Georgia. Additionally, Tournated's monetization tools have helped increase the federation's treasury, providing us with new revenue streams.",
       name: "Emiliano Zanon",
       role: "Officiating Manager of GTF",
       avatar: "https://i.imgur.com/wjFfNEm.jpg",
     },
     lv: {
-      text: "Tournated.com revolucionēja mūsu turnīru vadību, racionalizējot procesus. Ļoti ieteicams nevainojamiem pasākumiem.",
+      text: "Pateicoties Tournated risinājumam, mēs beidzot varējām palaist savu sporta vadības platformu un nacionālo līgu, ievērojami paātrinot tenisa izaugsmi Gruzijā. Turklāt Tournated monetizācijas rīki ir palīdzējuši palielināt federācijas budžetu, nodrošinot mums jaunas ieņēmumu plūsmas.",
       name: "Emiliano Zanon",
       role: "GTF tiesnešu vadītājs",
       avatar: "https://i.imgur.com/wjFfNEm.jpg",
@@ -29,14 +29,14 @@ export const testimonialsData = [
   },
   {
     en: {
-      text: "Tournated.com brilliantly met custom requests for seamless tournament management. Highly satisfied.",
+      text: "Before Tournated, there was no affordable, customizable, and automated solution available for managing sport fishing events. Tournated's solutions save our officials a significant amount of time and reduce stress, while also being highly convenient for the athletes. ",
       name: "Kristaps Kotans",
       role: "Boats discipline manager of LMSF",
       avatar:
         "https://www.lpkomiteja.lv/wp-content/uploads/2017/10/Kristaps_Kotans.jpg",
     },
     lv: {
-      text: "Tournated.com lieliski izpildīja pielāgotus pieprasījumus nevainojamai turnīru vadībai. Ļoti apmierināts.",
+      text: "Pirms Tournated nebija pieejams tik pielāgojams un automatizēts risinājums sporta makšķerēšanas pasākumu pārvaldībai. Tournated risinājumi ievērojami ietaupa mūsu tiesnešu laiku un samazina stresu, vienlaikus nodrošinot ļoti ērtu pieredzi sportistiem. ",
       name: "Kristaps Kotans",
       role: "LMSF laivu disciplīnas vadītājs",
       avatar:
